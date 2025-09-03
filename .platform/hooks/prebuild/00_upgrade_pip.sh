023ed253ef56349b19ff1b7e49dfbddc0059b0b2
Sana Haidar <sanahaidar@Sanas-MacBook-Air.local>
2025-09-03 10:34:03 +0200

    pip instead of pip

--- .platform/hooks/prebuild/00_upgrade_pip.sh
+++ .platform/hooks/prebuild/00_upgrade_pip.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Check if pip is installed. If not, install it.
-if ! command -v pip3 &> /dev/null; then
-    echo "pip3 not found, installing..."
+if ! command -v pip &> /dev/null; then
+    echo "pip not found, installing..."
     curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 fi
 
 # Now that pip is confirmed to be installed, proceed with upgrade
-python3 -m pip3 install --upgrade pip3 setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
